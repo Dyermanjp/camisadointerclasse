51b4f3deba8c713bdcb5c78ec2c4b3f1d6c5afa3
jp <joaopedro48145@gmail.com>
2026-08-26 22:59:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -280,8 +280,9 @@ els.adminLoginForm.addEventListener('submit', async (e) => {
     await auth.signInWithEmailAndPassword(ADMIN_EMAIL, password);
     els.adminPassword.value = '';
   } catch (error) {
-    console.error(error);
-    setAdminLoginMessage('Usuário ou senha incorretos.', true);
+  console.error(error);
+  setAdminLoginMessage(`${error.code}: ${error.message}`, true);
+}
   } finally {
     els.adminLoginBtn.disabled = false;
     els.adminLoginBtn.textContent = 'Entrar';
